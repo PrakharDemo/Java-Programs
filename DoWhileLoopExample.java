e706d123b69f71603424ee7b5957b73924a7f038
Prakhar.Srivastava1988 <prakhar.jss.mech@gmail.com>
2019-12-12 19:18:32 +0530

    Add files via upload
    shows the implementation of a do-While loop, to count the sum of digits of a number.

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class DoWhileLoopExample 
+{
+	public static void main(String[] args) 
+	{
+	    // This program displays the sum of digits of a number, using the do-While loop
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the number whose sum of digits is required :");
+		int number = input.nextInt();
+		int copy = number;
+		int i, sum = 0;
+		do
+		{
+			i = number%10;
+			sum += i;
+			number=number/10;
+		}
+		while (number >0);
+		System.out.println("The sum of digits of the number " + copy + " is : " + sum);
+	}
+
+}
