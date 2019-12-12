b058df95ea331fa9c2cdbe6968959861539ec760
Prakhar.Srivastava1988 <prakhar.jss.mech@gmail.com>
2019-12-12 19:09:26 +0530

    Add files via upload
    This program shows how we can request an Input from the user, through the use of Scanner class.

---
+++ UserInput.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class UserInput 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		// Getting a float value as an input
+		System.out.println("Please enter a decimal value as input :");
+		float myFloat = input.nextFloat();
+		System.out.println("The entered float value is : " + myFloat);
+
+		// Getting a double value as an input
+    	System.out.print("Please enter a double value as input :");
+    	double myDouble = input.nextDouble();
+    	System.out.println("The entered Double value is : " + myDouble);
+    	
+    	// Getting String input
+    	System.out.print("Please enter a text value as input: ");
+    	String myString = input.next();
+    	System.out.println("The entered text is " + myString);
+	}
+
+}
