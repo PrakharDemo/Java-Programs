fe7d058a2a20cdfcd50b220cbbbf70870182a47d
PrakharDemo <51440518+PrakharDemo@users.noreply.github.com>
2019-12-12 18:04:31 +0530

    Add files via upload
    This class file shows Primitive data types available in Java.

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,24 @@
+
+public class PrimitiveDatatypes 
+{
+	public static void main(String args[])
+	{
+		int myInt = 1000000000;
+		short myShort = 10000;
+		long myLong = 25648972;
+		double myDouble = 5452125445454.45656662425;
+		float myFloat = 56.35f;
+		char myChar = 'n';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		
+		System.out.println(myInt);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myByte);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+	}
+}
