facc2df525084ee57e037a2f64cb4f5bc183f266
Prakhar.Srivastava1988 <prakhar.jss.mech@gmail.com>
2019-12-18 11:35:59 +0530

    Default Constructor Demo

---
+++ Chocolate.java
@@ -0,0 +1,52 @@
+public class Chocolate 
+{
+	private int barCode;
+	private String name;
+	private int weight;
+	private int cost;
+	
+	public Chocolate() 
+	{
+		
+	}
+	
+	public int getBarCode() 
+	{
+		return barCode;
+	}
+
+	public void setBarCode(int barCode) 
+	{
+		this.barCode = barCode;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public int getWeight() 
+	{
+		return weight;
+	}
+
+	public void setWeight(int weight) 
+	{
+		this.weight = weight;
+	}
+
+	public int getCost() 
+	{
+		return cost;
+	}
+
+	public void setCost(int cost) 
+	{
+		this.cost = cost;
+	}
+}
